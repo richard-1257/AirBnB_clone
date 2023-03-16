3918dcc42950b946748181bb2d3e34be8b91dfcb
richard <richardsifeanyi@gmail.com>
2023-03-16 13:49:26 -0700

    test models

--- models/city.py
+++ models/city.py
@@ -5,7 +5,6 @@ from models.base_model import BaseModel
 
 class City(BaseModel):
     """Represent a city.
-
     Attributes:
         state_id (str): The state id.
         name (str): The name of the city.
@@ -13,3 +12,4 @@ class City(BaseModel):
 
     state_id = ""
     name = ""
+
